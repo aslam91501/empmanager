be896a4a22d70d152d5a35ea360b2c8376c4f714
Muhammed Aslam <aslam91501@gmail.com>
2025-03-24 22:39:10 +0530

    first commit

---
+++ src/main/java/com/aslam/employee/Employee.java
@@ -0,0 +1,5 @@
+package com.aslam.employee;
+
+public class Employee {
+
+}
---
+++ src/test/java/com/aslam/empmanager/EmpmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aslam.empmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
